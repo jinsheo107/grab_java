4cf76dbda38964ae32b58f368d613553bec453dd
jinsheo <jinsheo107@gmail.com>
2024-07-16 12:41:33 +0900

    [진서형] common 생성

---
+++ grab_java/src/main/java/com/grab/common/filter/EncryptFilter.java
@@ -0,0 +1,36 @@
+package com.grab.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+
+// 비밀번호 암호화를 사용할 페이지 설정
+@WebFilter(servletNames= {"userCreateEnd", "userLoginEnd", "userEditEnd"})
+public class EncryptFilter extends HttpFilter implements Filter {
+
+    public EncryptFilter() {
+        super();
+    }
+    
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// PasswordEncodingWrapper에서 비밀번호 암호화
+		PasswordEncodingWrapper pew = new PasswordEncodingWrapper((HttpServletRequest)request);
+		chain.doFilter(pew, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		
+	}
+
+}
